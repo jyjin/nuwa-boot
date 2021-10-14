d39813e53a3f3b69cae8d2f4d995f6e67085c454
金建业 <jianye.jin@hand-china.com>
2021-10-14 09:45:20 +0800

    [IMP] test hot line

--- src/react/index.js
+++ src/react/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import './index.scss'
 import './index.less'
 const App = () => {
-  return <div>Hello choerodon-ui DataSet 123 {DEV}</div>
+  return <div>Hello choerodon-ui DataSet 222 {DEV}</div>
 }
 
 export default App
