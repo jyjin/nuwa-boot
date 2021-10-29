d14f7f7f72a8759def36df4d6fa69e7d9e7eda34
金建业 <jianye.jin@hand-china.com>
2021-10-29 17:46:51 +0800

    [ADD] 增加react hash router

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route, Switch, withRouter, HashRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import ReactDOM from 'react-dom';
 import Demo1 from './react/demo1';
 import Demo2 from './react/demo2';
@@ -9,12 +11,33 @@ const Header = () => {
   </div>
 }
 
+// const getComp = ()=> import('./react.demo1.js')
+// const stream = []
+// getComp().then((module)=>{
+//   stream.push(module.default || module)
+// })
+
+// ReactDOM.render(
+//   <div>
+//     <Header />
+//     <h1 align='center'>Welcome to Nuwa! </h1>
+//     <Demo1 />
+//     <Demo2 />
+//   </div>,
+//   document.getElementById('root')
+// );
+
+const Spin = () => <div>Loading...</div>
+
 ReactDOM.render(
-  <div>
-    <Header />
-    <h1 align='center'>Welcome to Nuwa! </h1>
-    <Demo1 />
-    <Demo2 />
-  </div>,
+  <Router hashHistory={createBrowserHistory}>
+    <Switch>
+      <Route exact path={`/demo1`} component={Demo1} />
+      <Route exact path={`/demo2`} component={Demo2} />
+      {/* <Route exact path={`${match.url}${match.url === '/' ? '' : '/'}undefined`} component={Spin} />
+      <Route path={match.url} component={AutoRouter} /> */}
+    </Switch>
+  </Router>
+  ,
   document.getElementById('root')
-);
+)
--- src/react/demo1.js
+++ src/react/demo1.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-
+const Header = () => {
+  return <div className={'header'} style={{ color: 'white' }}>
+    <a>NUWA</a>
+  </div>
+}
 export default class Demo1 extends Component {
   render() {
     return (
       <div>
+        <Header />
+        <h1 align='center'>Welcome to Nuwa! </h1>
         <h2 align='center'>Nuwa is a micro and lightly frontend frameword base on React.</h2>
         {/* <h2 align='center'>欢迎加入女娲 </h2>
         <h3 align='center'>女娲——轻量前端微服务脚手架.</h3> */}
